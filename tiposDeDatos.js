6a3e2f36fe34312cd55965e3dea6dd35bb6311c7
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-12 16:38:36 -0500

    Se borra el archivo Factorial.html, se agrega el src para el archivo Javascript, se crea archivo JS donde muestran los tipos de datos que se manejan en JavaScript

---
+++ tiposDeDatos.js
@@ -0,0 +1,25 @@
+/**
+ * Tipos de  datos
+ * -Uso de operador typeof
+ * -Muestra datos desde la consola del navegador
+ */
+
+//Number - Para todos los tipos de numeros. Enteros y decimales
+console.log(typeof 50)
+
+//String - Cadena de texto
+console.log(typeof 'Alan')
+
+//Boolean - False o True(palabras reservadas)
+console.log(typeof true)
+
+//Function
+console.log(typeof function() {})
+
+//Object
+console.log(typeof {a:1})
+
+//Undefined
+console.log(typeof undefined)
+
+
