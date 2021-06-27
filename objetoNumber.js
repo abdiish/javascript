1118ca23cfd29aac0002bbf51b8234b1dd966cfb
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-27 13:47:50 -0500

    Objeto número, uso de algunas funciones.

---
+++ objetoNumber.js
@@ -0,0 +1,24 @@
+/**
+ *  Objetos de tipo número
+ */
+
+var a = 10.4546789;
+/* var b = new Number(10);
+
+//Valoeres decimales
+console.log(a.toFixed(5));
+
+a = a.toString();
+console.log(a); */
+console.log(a);
+//Presición
+console.log(a.toPrecision(4));
+
+a = 10;
+a *=  "f";
+console.log(a);
+
+
+var b = new Number("20");
+console.log(b);
+console.log(b.valueOf());
